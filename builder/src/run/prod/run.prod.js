6b2919fdd2f4916bb76f25c055ec7e9352727e4c
xilanhuadechuntian <Lensgcx@163.com>
2019-03-18 22:18:16 +0800

    run

---
+++ builder/src/run/prod/run.prod.js
@@ -0,0 +1,5 @@
+'use strict';
+const webpackConfig = require('../../env/webpack.prod.conf');
+const runbase = require('./run.base');
+
+runbase(webpackConfig);
---
+++ builder/src/run/prod/run.prod.plugin.js
@@ -0,0 +1,5 @@
+'use strict';
+const webpackConfig = require('../../env/webpack.prod.plugin.conf.js');
+const runbase = require('./run.base');
+
+runbase(webpackConfig);
