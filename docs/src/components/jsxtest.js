04e23f0490ebc981c97c76b6ec34ed00a7d1e64a
xilanhuadechuntian <Lensgcx@163.com>
2019-03-18 22:20:23 +0800

    jsxtest
    jsxtest

---
+++ docs/src/components/jsxtest.js
@@ -0,0 +1,24 @@
+/*
+ * pagination modules
+ * created: 2018/6/30.
+ * author: Broccoli spring( 高仓雄 - gcx )
+ * copyright (c) 2018 Broccoli spring( gcx )
+ */
+export default {
+  name: 'jsx-test',
+  data() {
+    return {
+      title: 'jsx - test ',
+    }
+  },
+  render(h) {
+    return (
+      <h1>{this.title}</h1>
+    )
+  },
+  created() {
+
+  },
+  methods: {},
+};
+
