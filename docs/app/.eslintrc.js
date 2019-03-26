171de6dd318805cb926e7a7265ab8c8532802e16
xilanhuadechuntian <Lensgcx@163.com>
2019-03-26 22:11:56 +0800

    eslintrc
    eslintrc

--- docs/app/.eslintrc.js
+++ docs/app/.eslintrc.js
@@ -16,11 +16,23 @@
 
 module.exports = {
   root: true,
+  //想要支持的JS语言选项
   parserOptions: {
-    parser: 'babel-eslint'
+    /**
+     * babel-eslint 解析器是一种使用频率很高的解析器，
+     * 因为现在很多公司的很多项目目前都使用了es6，为了兼容性考虑基本都使用babel插件对代码进行编译。
+     * 而用babel编译后的代码使用 babel-eslint 这款解析器可以避免不必要的麻烦。
+     * npm install --save-dev babel-eslint
+     */
+    parser: 'babel-eslint',
+    //默认为script
+    sourceType: "module",
   },
+  //代码运行的环境，每个环境都会有一套预定义的全局对象，不同环境可以组合使用
   env: {
     browser: true,
+    node: true,
+    es6: true
   },
   /**
    * 1、使用第三方分享的，我们一般需要安装相关的插件代码如下:
@@ -50,13 +62,6 @@ module.exports = {
     'html/html-extensions': ['.html', '.vue'],
     'html/indent': '+2',
   },
-  /**
-   * babel-eslint 解析器是一种使用频率很高的解析器，
-   * 因为现在很多公司的很多项目目前都使用了es6，为了兼容性考虑基本都使用babel插件对代码进行编译。
-   * 而用babel编译后的代码使用 babel-eslint 这款解析器可以避免不必要的麻烦。
-   * npm install --save-dev babel-eslint
-   */
-  parser: 'babel-eslint',
   // add your custom rules here
   rules: {
     // allow async-await
