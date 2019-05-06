7b7858d624c3b8978a24a86a89d063dd81a019b9
xilanhuadechuntian <Lensgcx@163.com>
2019-05-06 14:32:15 +0800

    Update webpack.dev.conf.js

--- builder/src/env/webpack.dev.conf.js
+++ builder/src/env/webpack.dev.conf.js
@@ -1,41 +1,47 @@
-'use strict';
-const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+"use strict";
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const utils = require('../utils');
-const config = require(utils.rootPath('env.param.config'));
-const baseWebpackConfig = require('../basic/webpack.base.conf');
+const utils = require("../utils");
+const config = require("../config");
+const baseWebpackConfig = require("../basic/webpack.base.conf");
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT && Number(process.env.PORT);
 
-const _is_mock = process.env.env_config === 'mock';
-const env = require(utils.rootPath('env.config'))[_is_mock ? 'mock_env' : 'dev_env'];
+const _is_mock = process.env.env_config === "mock";
+const env = require(utils.rootPath("env.config"))[_is_mock ? "mock_env" : "dev_env"];
 
 const devWebpackConfig = {
-  mode: 'development', // 通过 mode 声明开发环境
+  mode: "development", // 通过 mode 声明开发环境
   module: {},
-  entry: config.dev.entry || baseWebpackConfig.entry,
+  entry: config.dev.entry,
+  output: {
+    path: config.dev.assetsRoot,
+    filename: "[name].js",
+    publicPath: config.dev.assetsPublicPath
+  },
+
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
 
-  // these devServer options should be customized in /config/env.param.config.js
+  // these devServer options should be customized in env.param.config.js
   devServer: {
     /**
      * 当使用内联模式(inline mode)时，会在开发工具(DevTools)的控制台(console)显示消息, 可能的值有 none, error, warning 或者 info（默认值）
      */
-    clientLogLevel: 'warning',
+    clientLogLevel: "warning",
     /**
      * 当使用 HTML5 History API 时，任意的 404 响应都可能需要被替代为 index.html。通过传入以下启用：
      * 通过传入一个对象，比如使用 rewrites 这个选项，此行为可进一步地控制：
      */
     historyApiFallback: {
       rewrites: [
-        {from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html')},
-      ],
+        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, "index.html") }
+      ]
     },
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
@@ -45,7 +51,7 @@ const devWebpackConfig = {
     open: config.dev.autoOpenBrowser,//是否自动打开浏览器
     //当出现编译器错误或警告时，在浏览器中显示全屏覆盖层。默认禁用。
     overlay: config.dev.errorOverlay
-      ? {warnings: false, errors: true}
+      ? { warnings: false, errors: true }
       : false,
     publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
@@ -56,10 +62,10 @@ const devWebpackConfig = {
     quiet: config.dev.quiet,
     stats: "errors-only", //stats: "errors-only"表示只打印错误：
     // before: require('../mock-server/index'),//引入mock/index.js
-    before: _is_mock ? require('../mock-server/index') : (app) => {
+    before: _is_mock ? require("../mock-server/index") : (app) => {
     },
     watchOptions: {
-      poll: config.dev.poll,
+      poll: config.dev.poll
     }
   },
   plugins: [
@@ -67,7 +73,7 @@ const devWebpackConfig = {
      * 定义process.env
      */
     new webpack.DefinePlugin({
-      'process.env': env
+      "process.env": env
     }),
     /**
      * 模块热替换插件
@@ -84,16 +90,16 @@ const devWebpackConfig = {
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: utils.getPropertyByEnv('templateSPA'),
+      filename: "index.html",
+      template: config.dev.templateSPA,
       inject: true
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: utils.rootPath(utils.getPropertyByEnv('staticPath')),
+        from: utils.rootPath(config.dev.staticPath),
         to: config.dev.assetsSubDirectory,
-        ignore: ['.*']
+        ignore: [".*"]
       }
     ])
   ]
