78a381e5dd7c886b456c833226ce7dbcf24364b6
xilanhuadechuntian <Lensgcx@163.com>
2019-05-06 14:32:13 +0800

    Update menu.sh

--- builder/src/menu.sh
+++ builder/src/menu.sh
@@ -1,35 +1,58 @@
 #Design by Broccoli spring( gcx-高仓雄 ) <Lensgcx@163.com>
+
 #!/usr/bin/env bash
 
 WORKDIR=$(dirname $0)
-echo ${WORKDIR}
-
 source ${WORKDIR}'/sh/menusource.sh'
 
-# judge parame and assign
+clear
+echo
+echo ${WORKDIR}
 
-if [ $1 = "root" ]
-   then
-   echo
-   echo "命令带有参数：$1，切换开发者模式，正在打开 root menu，请稍后..."
-   echo
-   TYPE="root"
-   startMenu
+function runByCommandNum {
+    if [  -n "$VARNUM" ]
+       then
+        echo
+        echo "命令带有参数："$VARNUM" ，直接启动对应服务 "$VARNUM" ，请稍后..."
+        echo
+
+          #定义参数 Menu_main_opt
+          Menu_main_opt="$VARNUM"
+          #执行函数，选择对应命令执行
+          Menu_main_command
+
+    else
+          echo
+          echo "命令不带有参数, 启用操作菜单界面，请稍后..."
+          echo
+
+          startMenu
+    fi
+}
+
+
+if [ "$1" = "root" ]
+    then
+    echo
+    echo "正在启动 $1 模式，请稍后..."
+
+    TYPE="root"
+    VARNUM=$2
+    runByCommandNum
+
+elif [ $1 = "developer" ]
+    then
+    echo
+    echo "正在启动 $1 模式，请稍后..."
+
+    TYPE="developer"
+    VARNUM=$2
+    runByCommandNum
+
 else
-  if [  -n "$1" ]
-     then
-      echo
-      echo "命令带有参数：$1，直接启动对应服务$1，请稍后..."
-      echo
-      echo
-        opt=${1};
-        runcommand
-  else
-        echo
-        echo "命令不带有参数, 启用操作菜单界面，请稍后..."
-        echo
-        echo
-        TYPE="main"
-        startMenu
-  fi
+    echo "当前模式：$1 不存在，采用默认模式 - developer "
+
+    TYPE="developer"
+    VARNUM=$2
+    runByCommandNum
 fi
