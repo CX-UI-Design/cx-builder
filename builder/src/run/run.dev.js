59055348f2a170bcda789d232231416bff0f2a6d
xilanhuadechuntian <Lensgcx@163.com>
2019-05-06 14:33:56 +0800

    Update run.dev.js

--- builder/src/run/run.dev.js
+++ builder/src/run/run.dev.js
@@ -1,9 +1,12 @@
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
-const portfinder = require('portfinder');
+const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+const portfinder = require("portfinder");
 
-const utils = require('../utils');
-const config = require(utils.rootPath('env.param.config'));
-const devWebpackConfig = require('../env/webpack.dev.conf');
+const utils = require("../utils");
+const config = require("../config");
+const devWebpackConfig = require("../env/webpack.dev.conf");
+
+console.log('\n');
+
 /**
  * start by Portfinder
  * 确保启动程序时，如果端口被占用时，会通过portfinder来发布新的端口，然后输出运行的host字符串
@@ -15,21 +18,23 @@ module.exports = new Promise((resolve, reject) => {
   portfinder.basePort = process.env.PORT || config.dev.port;
   portfinder.getPort((err, port) => {
     if (err) {
-      reject(err)
+      reject(err);
     } else {
       // publish the new Port, necessary for e2e tests
       process.env.PORT = port;
       // add port to devServer config
       devWebpackConfig.devServer.port = port;
-      console.log(port);
+
+      // console.log(port);
+
       // Add FriendlyErrorsPlugin
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
           // notes: [`Create by Broccoli spring( gcx ) <Lensgcx@163.com>: https://github.com/Lensgcx`],\
           notes: [`Create by Broccoli spring( gcx )`],
           messages: [
-            `Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`,
-          ],
+            `Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`
+          ]
         },
         onErrors: config.dev.notifyOnErrors
           ? utils.createNotifierCallback()
@@ -37,5 +42,5 @@ module.exports = new Promise((resolve, reject) => {
       }));
       resolve(devWebpackConfig);
     }
-  })
+  });
 });
