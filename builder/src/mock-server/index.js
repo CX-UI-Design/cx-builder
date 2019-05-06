f0b35fab13349e7f6cdf4bcad47fcfb5c18d0d29
xilanhuadechuntian <Lensgcx@163.com>
2019-05-06 14:33:45 +0800

    Update index.js

--- builder/src/mock-server/index.js
+++ builder/src/mock-server/index.js
@@ -1,19 +1,19 @@
 const path = require('path');
 const bodyParser = require('body-parser');
-const config = require(path.resolve('env.param.config'));
-const mockPath = config.base.mockPath || './mock';
+const config = require('../config');
 
 /**
  * 注入 mock 路径下所有配置文件
  * https://www.npmjs.com/package/require-all
  */
 const controllers = require('require-all')({
-  dirname: path.resolve(mockPath),
+  dirname: path.resolve(config.base.mockPath),
 });
 
 //将注入的对象取value转换为数组
 const mockModule = Object.values(controllers) || [];
-console.log(mockModule);
+
+// console.log(mockModule);
 
 module.exports = function (app) {
 
@@ -25,7 +25,9 @@ module.exports = function (app) {
       const _method = key.split(' ')[0].toLowerCase();//mock 请求方法
       const _url = key.split(' ')[1];//mock 请求地址
       const _callback = module[key];//mock 回调函数
-      console.log(_method, _url);
+
+      // console.log(_method, _url);
+
       app[_method](_url, _callback);
     });
   })
