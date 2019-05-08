ae37af554548945a771186299d25a72fe523f82c
xilanhuadechuntian <Lensgcx@163.com>
2019-05-08 15:53:19 +0800

    Update plugin-config.js

--- builder/src/config/envconfig/plugin-config.js
+++ builder/src/config/envconfig/plugin-config.js
@@ -16,6 +16,11 @@ module.exports = {
     assetsSubDirectory: "static",// 编译输出的二级目录
     staticPath: "./static",//静态文件抽取static所在路径
 
+    /**
+     * 如果我们想引用一个库，但是又不想让webpack打包，并且又不影响我们在程序中以CMD、AMD或者window/global全局等方式进行使用，
+     * 那就可以通过配置externals。这个功能主要是用在创建一个库的时候用的，但是也可以在我们项目开发中充分使用。
+     */
+    externals:{},
 
     // https://webpack.js.org/configuration/devtool/#production
     devtool: "#source-map",
