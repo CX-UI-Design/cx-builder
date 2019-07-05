8a0107fc09456274f03762f124c6c1fecf138291
xilanhuadechuntian <Lensgcx@163.com>
2019-07-05 18:59:43 +0800

    Update prod-config.js

--- builder/src/config/envconfig/prod-config.js
+++ builder/src/config/envconfig/prod-config.js
@@ -8,6 +8,8 @@ module.exports = {
     assetsPublicPath: "/", // 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
     assetsRoot: path.resolve("./dist"), // 编译输出的静态资源路径
     assetsSubDirectory: "static", // 编译输出的二级目录
+    cssExtractPublicPath:'./',//MiniCssExtractPlugin 路径
+
     favicon: path.resolve("./favicon.ico"), //favicon
 
     templateSPA: "./index.html",//模板文件抽取所在路径
