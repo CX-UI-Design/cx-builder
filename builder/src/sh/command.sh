22ad43a9b03038009a84d860318006401c7c4639
xilanhuadechuntian <Lensgcx@163.com>
2019-07-10 17:36:10 +0800

    Update command.sh

--- builder/src/sh/command.sh
+++ builder/src/sh/command.sh
@@ -7,45 +7,50 @@ min_suffix='' #压缩后缀名
 
 #1、run dev
 function Fn_run_dev {
-  NODE_ENV=development env_config=dev webpack-dev-server --colors --inline --progress --config ${WORKDIR}/run/run.dev${min_suffix}.js
+  NODE_ENV=development env_config=dev process_by_sign=dev_env webpack-dev-server --colors --inline --progress --config ${WORKDIR}/run/run.dev${min_suffix}.js
 }
 
-#2、run mock
-function Fn_run_mock {
-  NODE_ENV=development env_config=mock webpack-dev-server --colors --inline --progress --config ${WORKDIR}/run/run.dev${min_suffix}.js
+#2、run local mock
+function Fn_run_local_mock {
+  NODE_ENV=development env_config=mock process_by_sign=mock_local_env webpack-dev-server --colors --inline --progress --config ${WORKDIR}/run/run.dev${min_suffix}.js
 }
 
-#3、run build
+#3、run cloud mock
+function Fn_run_cloud_mock {
+  NODE_ENV=development env_config=dev process_by_sign=mock_cloud_env webpack-dev-server --colors --inline --progress --config ${WORKDIR}/run/run.dev${min_suffix}.js
+}
+
+#4、run build
 function Fn_run_prod {
-  cross-env NODE_ENV=production env_config=prod node ${WORKDIR}/run/prod/run.prod${min_suffix}.js
+  cross-env NODE_ENV=production env_config=prod process_by_sign=prod_env node ${WORKDIR}/run/prod/run.prod${min_suffix}.js
 }
 
-#4、run build plugin
+#5、run build plugin
 function Fn_run_prod_plugin {
-  cross-env NODE_ENV=production env_config=plugin node ${WORKDIR}/run/prod/run.prod.plugin${min_suffix}.js
+  cross-env NODE_ENV=production env_config=plugin process_by_sign=prod_env node ${WORKDIR}/run/prod/run.prod.plugin${min_suffix}.js
 }
 
-#5、run prettier code
+#6、run prettier code
 function Fn_run_prettier {
   cross-env node ${WORKDIR}/prettier/prettier${min_suffix}.js
 }
 
-#6、run lint js code
+#7、run lint js code
 function Fn_run_lint {
   eslint --ext .js --ext .jsx --ext .ts --ext .tsx --ext .vue src mock
 }
 
-#7、run fix js code
+#8、run fix js code
 function Fn_run_lint_fix {
   eslint --fix --ext .js --ext .jsx --ext .ts --ext .tsx --ext .vue src mock
 }
 
-#8、run lint style code
+#9、run lint style code
 function Fn_run_lint_style {
  stylelint src/**/*.{vue,css,scss,less,sass}
 }
 
-#9、run fix style code
+#10、run fix style code
 function Fn_run_lint_style_fix {
  stylelint src/**/*.{vue,css,scss,less,sass} --fix
 }
