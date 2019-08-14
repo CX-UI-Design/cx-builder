e47df0314cdd1cadcadc7e538feba8514773d7a2
xilanhuadechuntian <Lensgcx@163.com>
2019-08-14 16:44:18 +0800

    Update run.prod.js

--- builder/src/run/prod/run.prod.js
+++ builder/src/run/prod/run.prod.js
@@ -1,5 +1,6 @@
-'use strict';
-const webpackConfig = require('../../env/webpack.prod.conf');
-const runbase = require('./run.base');
+"use strict";
+const webpackConfig = require("../../env/webpack.prod.conf");
+const runbase = require("./run.base");
 
 runbase(webpackConfig);
+
