9c7d1cd5664ae25b3342671aa0e6f5d5c32a401a
xilanhuadechuntian <Lensgcx@163.com>
2019-08-19 17:32:23 +0800

    Create index.js

---
+++ builder/src/tools/reinstall/index.js
@@ -0,0 +1,18 @@
+/**
+ * Reinstall node_modules
+ */
+const path = require("path");
+const shell = require("shelljs");
+const signale = require("signale");
+const reinstall = path.resolve(__dirname, "./reinstall.sh");
+const shUtilsPath = path.resolve(__dirname, "../../sh/utils.sh");
+
+shell.exec('clear');
+
+signale.start(`Start to reinstall node_modules...\n`);
+
+shell.exec(`${reinstall} ${shUtilsPath}`);
+
+signale.success(`Reinstall successful...\n`);
+
+
