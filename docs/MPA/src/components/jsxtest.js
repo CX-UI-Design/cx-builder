fe12cfb5d0e89fd292b5f398d43a76babf300d1b
xilanhuadechuntian <Lensgcx@163.com>
2019-09-25 14:11:55 +0800

    docs MPA

---
+++ docs/MPA/src/components/jsxtest.js
@@ -0,0 +1,19 @@
+/*
+ * pagination modules
+ * created: 2018/6/30.
+ * author: Broccoli spring( 高仓雄 - gcx )
+ * copyright (c) 2018 Broccoli spring( gcx )
+ */
+export default {
+  name: 'jsx-test',
+  data() {
+    return {
+      title: 'jsx - test123123 ',
+    };
+  },
+  render(h) {
+    return <h1>{this.title}</h1>;
+  },
+  created() {},
+  methods: {},
+};
