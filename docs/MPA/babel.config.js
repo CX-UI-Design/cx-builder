6a0efe89a84ee3ad120896ee251b89aae496c5b5
xilanhuadechuntian <Lensgcx@163.com>
2019-09-25 15:54:55 +0800

    Update babel.config.js

--- docs/MPA/babel.config.js
+++ docs/MPA/babel.config.js
@@ -1,33 +1,35 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         modules: false,
         targets: {
           browsers: [
-            "> 1%",
-            "last 2 versions",
-            "not ie <= 8"
-          ]
-        }
-      }
-    ]
+            '> 1%',
+            'last 2 versions',
+            'not ie <= 8',
+          ],
+        },
+      },
+    ],
   ],
   plugins: [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-syntax-dynamic-import",
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    "@babel/plugin-syntax-jsx",
-    "@vue/babel-plugin-transform-vue-jsx",
-    "jsx-v-model"
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-transform-modules-commonjs',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-syntax-jsx',
+    '@vue/babel-plugin-transform-vue-jsx',
+    'jsx-v-model',
   ],
   env: {
     development: {
       plugins: [
-        "dynamic-import-node"
-      ]
-    }
-  }
+        'dynamic-import-node',
+      ],
+    },
+  },
 };
