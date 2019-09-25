0471b1f8fca5e6d1cdf442db7962e3381d65e654
xilanhuadechuntian <Lensgcx@163.com>
2019-09-25 14:09:25 +0800

    Update index.js

--- builder/src/config/index.js
+++ builder/src/config/index.js
@@ -7,6 +7,7 @@ const _prod_config = require("./envconfig/prod-config");
 const _plugin_config = require("./envconfig/plugin-config");
 const _tools_config = require("./envconfig/tools-config");
 
+
 const defaultConfig = merge(_base_config, _dev_config, _prod_config, _plugin_config, _tools_config);
 
 const customConfig = require(path.resolve("env.param.config"));
@@ -16,9 +17,28 @@ if (!customConfig) {
   throw "not found custom config file - env.param.config.js";
 }
 
-const c = merge(defaultConfig, customConfig);
+/**
+ * change default config base on custom config state
+ * @param defaultConfig
+ * @param customConfig
+ * @returns {*}
+ */
+function filterChangeConfig(defaultConfig, customConfig) {
+  /*
+   * 提取公共代码的配置项，一旦自定义配置后，将全部覆盖原有默认配置，而不是根据字段合并
+   */
+  if (customConfig.prod.splitChunks) {
+    defaultConfig.prod.splitChunks = {};
+  }
+  return defaultConfig;
+}
+
+
+const c = merge(filterChangeConfig(defaultConfig, customConfig), customConfig);
+
 
 // console.log("最终配置对象:");
 // console.log(c);
+
 
 module.exports = c;
