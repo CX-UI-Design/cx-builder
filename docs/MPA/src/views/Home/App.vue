e494fe99da7b6e283eb235d95f2534910be0086d
xilanhuadechuntian <Lensgcx@163.com>
2019-09-25 15:55:14 +0800

    Update App.vue

--- docs/MPA/src/views/Home/App.vue
+++ docs/MPA/src/views/Home/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
+    <h1>Home</h1>
     <nav>
       <ul>
         <li>Go to</li>
         <li>
-          <a href="../About/index.html">
-            About Us
-          </a>
+          <a href="../index.html">index</a>
+        </li>
+        <li>
+          <a href="../About/index.html">About Us</a>
         </li>
       </ul>
     </nav>
@@ -14,46 +16,48 @@
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+  import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: "Home",
+    components: {
+      HelloWorld
+    }
+  };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style rel="stylesheet/scss" lang="scss">
+  @import '../../assets/css/common';
 
-.short-it {
-  position: relative;
-
-  overflow-x: hidden;
-  overflow-y: auto;
-  max-height: 300px;
-
-  + div {
-    position: absolute;
-    bottom: 0;
-    z-index: 1;
-
-    display: block;
-    width: 100%;
-    height: 40px;
-    background-image: linear-gradient(to bottom, transparent, #fff);
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
   }
-}
 
-nav ul {
-  margin-top: 30px;
-}
+  .short-it {
+    position: relative;
+
+    overflow-x: hidden;
+    overflow-y: auto;
+    max-height: 300px;
+
+    + div {
+      position: absolute;
+      bottom: 0;
+      z-index: 1;
+
+      display: block;
+      width: 100%;
+      height: 40px;
+      background-image: linear-gradient(to bottom, transparent, #fff);
+    }
+  }
+
+  nav ul {
+    margin-top: 30px;
+  }
 </style>
