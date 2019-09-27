ff2fe557539e5e64f1fbbeeb6ea2756da0c4085b
xilanhuadechuntian <Lensgcx@163.com>
2019-09-27 10:56:51 +0800

    Update env.param.config.js

--- docs/SPA/env.param.config.js
+++ docs/SPA/env.param.config.js
@@ -7,7 +7,79 @@ module.exports = {
     useEslint: false
   },
   prod: {
-    useEslint: false
+    useEslint: false,
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: 5,
+      cacheGroups: {
+        libs: {
+          name: "chunk-libs",
+          test: module => {
+            if (module.resource) {
+              const include = [/[\\/]node_modules[\\/]/].every(reg => {
+                return reg.test(module.resource);
+              });
+              const exclude = [/[\\/]node_modules[\\/](vue|element-ui|neap-ui|)/].some(reg => {
+                return reg.test(module.resource);
+              });
+              return include && !exclude;
+            }
+            return false;
+          },
+          priority: -10,
+          chunks: "initial"
+        },
+
+
+        "vue": {
+          name: "chunk-vue",
+          priority: 20,
+          test: /[\\/]node_modules[\\/]vue[\\/]/
+        },
+
+        "element-ui": {
+          name: "chunk-element-ui",
+          priority: 20,
+          test: /[\\/]node_modules[\\/]element-ui[\\/]/
+        },
+
+
+        "neap-ui": {
+          name: "chunk-neap-ui",
+          priority: 20,
+          test: /[\\/]node_modules[\\/]neap-ui[\\/]/
+        },
+
+        "biz-components": {
+          name: "chunk-biz-components",
+          test: /[\\/]node_modules[\\/]neap-test1[\\/]injection[\\/]repositories[\\/]NS_biz_components[\\/]/,
+          priority: 20
+        }
+
+
+        // 'neap-test1': {
+        //   name: 'chunk-neap-core',
+        //   priority: 20,
+        //   test: /[\\/]node_modules[\\/]neap-test1[\\/]/,
+        // },
+
+        // commons: {
+        //   name: 'chunk-commons',
+        //   test: /[\\/]node_modules[\\/]neap-test1[\\/]injection[\\/]repositories[\\/]NS_biz_components[\\/]/,
+        //   minChunks: 2, // 最小公用次数
+        //   priority: 30,
+        //   reuseExistingChunk: true,
+        // },
+
+        // commons: {
+        //   name: 'chunk-commons',
+        //   test: resolve('src/components'), // 可自定义拓展你的规则
+        //   minChunks: 3, // 最小公用次数
+        //   priority: 5,
+        //   reuseExistingChunk: true,
+        // },
+      }
+    },
   },
   base: {
     mixinPalette: {
