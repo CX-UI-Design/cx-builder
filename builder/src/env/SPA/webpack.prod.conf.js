3de29ce354f1b9f100fca26dfa4e57de53102ef1
xilanhuadechuntian <Lensgcx@163.com>
2019-11-29 10:14:11 +0800

    Update webpack.prod.conf.js

--- builder/src/env/SPA/webpack.prod.conf.js
+++ builder/src/env/SPA/webpack.prod.conf.js
@@ -8,7 +8,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 
 const utils = require("../../utils/index");
@@ -83,8 +84,8 @@ const prodWebpackConfig = merge(baseWebpackConfig, {
       // chunksSortMode: "dependency",
 
       templateParameters: {
-        BASE_URL: config.prod.assetsPublicPath + config.prod.assetsSubDirectory,
-      },
+        BASE_URL: config.prod.assetsPublicPath + config.prod.assetsSubDirectory
+      }
     }),
 
     new ScriptExtHtmlWebpackPlugin({
@@ -128,22 +129,49 @@ const prodWebpackConfig = merge(baseWebpackConfig, {
      * 在多页面项目中，盲目设置，会造成所依赖引用的js模块无法正常运行使用
      **/
     runtimeChunk: "single",
+
     /**
      * webpack4只需要设置 mode:produciton，默认optimization.minimize是true,所以js可以自动帮你打包混淆js代码.
      * 但是自定义minimizer后，webpack默认配置会取消掉,所以还需要添加 uglifyjs压缩js.
      */
     minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          mangle: {
-            //支持safari10且修复，2.0.0 版本之后移除
-            // safari10: true,
-          }
-        },
+
+      // new UglifyJsPlugin({
+      //   uglifyOptions: {
+      //     mangle: {
+      //       //支持safari10且修复，2.0.0 版本之后移除
+      //       // safari10: true,
+      //     },
+      //     compress: {
+      //       warnings: false,
+      //       drop_debugger: true,
+      //       drop_console: true
+      //     }
+      //   },
+      //   sourceMap: config.prod.prodJsSourceMap,
+      //   cache: true,
+      //   parallel: true
+      // }),
+
+      /**
+       * 如果代码里面包含了ES6的语法，则uglifyjs不支持，且uglifyjs对去console支持不好，
+       * 故而用terser-webpack-plugin替换掉uglifyjs-webpack-plugin
+       */
+      new TerserPlugin({
+        parallel: true, // 开启多进程压缩
+        cache: true, // 开启缓存(压缩过的不压缩)
         sourceMap: config.prod.prodJsSourceMap,
-        cache: true,
-        parallel: true
+        terserOptions: {
+          warnings: false,
+          compress: {
+            drop_console: true, //去除 console.log
+            drop_debugger: false, //去除 debugger
+            pure_funcs: ["console.log"] // 移除console
+          }
+        }
       }),
+
+
       // Compress extracted CSS. We are using this plugin so that possible
       // duplicated CSS from different components can be deduped.
       //优化css文件的，主要就是压缩css代码
