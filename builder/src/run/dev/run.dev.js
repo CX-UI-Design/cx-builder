544311ea0cd6f82193b314b6776ab3659f7bce00
xilanhuadechuntian <Lensgcx@163.com>
2021-01-09 18:50:33 +0800

    Update run.dev.js

--- builder/src/run/dev/run.dev.js
+++ builder/src/run/dev/run.dev.js
@@ -1,15 +1,15 @@
 require('@babel/plugin-transform-modules-commonjs');
-
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const portfinder = require('portfinder');
 
 const utils = require('../../utils/index');
 const config = require('../../config/index');
-const devMode = config.dev.mode;
 
+const hs = require('../hookScript');
 const SPA_webpackConfig = require('../../env/SPA/webpack.dev.conf'); // SPA prod config
 const MPA_webpackConfig = require('../../env/MPA/webpack.dev.conf'); // MPA prod config
 
+const devMode = config.dev.mode;
 let devWebpackConfig = null;
 
 if (['SPA', 'spa'].includes(devMode)) {
@@ -18,12 +18,22 @@ if (['SPA', 'spa'].includes(devMode)) {
   devWebpackConfig = MPA_webpackConfig;
 }
 
-const hs = require('../hookScript');
+//dev编译构建计时开始：初始时间
+const devStartTime = process.hrtime.bigint();
 
-console.log('\n');
+//前置钩子 计时开始：初始时间
+const hookScriptStartTime = process.hrtime.bigint();
 
 //run pre script
 hs.runHookScript('pre');
+
+//前置钩子 计时结束：结束时间
+const hookScriptEndTime = process.hrtime.bigint();
+
+//前置钩子 运行时间间隔
+const hookScriptTimeInterval = utils.timeIntervalFn(hookScriptStartTime, hookScriptEndTime, 's', 2);
+
+console.log('\n');
 
 /**
  * start by Portfinder
@@ -57,6 +67,17 @@ module.exports = new Promise((resolve, reject) => {
           },
           onErrors: config.dev.notifyOnErrors ? utils.createNotifierCallback() : undefined,
         })
+      );
+
+      //dev编译构建计时结束：结束时间
+      const devEndTime = process.hrtime.bigint();
+      //dev编译 运行时间间隔
+      const devTimeInterval = utils.timeIntervalFn(devStartTime, devEndTime, 's', 2);
+
+      console.log(
+        chalk.cyan(
+          `  Compile cost ${devTimeInterval} s , Pre hook script run cost ${hookScriptTimeInterval} s . \n`
+        )
       );
 
       resolve(devWebpackConfig);
