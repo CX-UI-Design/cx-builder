f8b955d5728dc77c8a4e4b836d5da83d44917a58
xilanhuadechuntian <Lensgcx@163.com>
2021-01-09 15:17:22 +0800

    Update env.param.config.js

--- env.param.config.js
+++ env.param.config.js
@@ -1,6 +1,18 @@
 "use strict";
 
 module.exports = {
+  base: {
+    //prettier code - 美化
+    prettier: {
+      switch: false,
+      files: [
+        "builder/src/**/*.{vue,less,scss,css,js,jsx,ts,tsx,json}",
+        "docs/MPA/src/**/*.{vue,less,scss,css,js,jsx,ts,tsx,json}",
+        "docs/plugin/src/**/*.{vue,less,scss,css,js,jsx,ts,tsx,json}",
+        "docs/SPA/src/**/*.{vue,less,scss,css,js,jsx,ts,tsx,json}",
+      ]
+    }
+  },
   tools: {
     publish: {
       prescript: [
